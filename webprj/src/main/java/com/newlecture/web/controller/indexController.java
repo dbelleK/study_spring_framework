6955e707f224913488d85ac5d270460a11af7523
dbelleK <kkddy1206@naver.com>
2022-05-09 02:11:45 +0900

    환경설정

---
+++ webprj/src/main/java/com/newlecture/web/controller/indexController.java
@@ -0,0 +1,27 @@
+package com.newlecture.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+public class indexController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		//System.out.println("index controller");
+		
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("data","Hello Spring MVC~");
+		//mv.setViewName("/WEB-INF/view/index.jsp");
+	
+
+		return mv;
+	}
+	
+	
+
+}
