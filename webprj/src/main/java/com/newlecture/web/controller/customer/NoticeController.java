baffaaca6d2df73a62f9bf7e965e2950bb78c785
MSI\kkddy <kkddy1206@naver.com>
2022-05-11 17:53:14 +0900

    QueryString 방식

--- webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ webprj/src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -3,9 +3,12 @@ package com.newlecture.web.controller.customer;
 import java.sql.SQLException;
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.newlecture.web.entity.Notice;
 import com.newlecture.web.service.NoticeService;
@@ -18,9 +21,13 @@ public class NoticeController {
 	private NoticeService noticeService;
 
 	@RequestMapping("list")
-	public String list() throws ClassNotFoundException, SQLException {
+	public String list(@RequestParam(name="p", defaultValue="1") String page) throws ClassNotFoundException, SQLException {
 		
-		List<Notice> list = noticeService.getList(1, "TITLE", ""); /*int page, String field, String query (""->모든것)*/ 
+		//String p = request.getParameter("p");
+		System.out.println("page : " + page);
+		
+		/*int page, String field, String query (""->모든것)*/ 
+		//List<Notice> list = noticeService.getList(1, "TITLE", ""); 
 
 		return "notice.list";
 	}
